571db6e5bea74f5e53eb693abab049d2a7abea9d
javaclass <javaclass@15000503CPU.fec.org>
2017-11-21 15:09:59 -0600

    my first Java program

---
+++ src/firstpackage/FirstProgram.java
@@ -0,0 +1,17 @@
+package firstpackage;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Welcome to CHIEFS KINGDOM!!!");
+		
+		
+
+	}
+	
+	
+}
+	
